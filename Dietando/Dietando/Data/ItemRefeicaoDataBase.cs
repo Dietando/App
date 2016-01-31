56111c8e8d85d320bbc75818a6272c037bcadfc9
DarciJunio <darcijsrodrigues@gmail.com>
2016-01-31 01:32:39 -0200

    Implantado SQLite

---
+++ Dietando/Dietando/Data/ItemRefeicaoDataBase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using SQLite.Net;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace Dietando
+{
+	public class ItemRefeicaoDataBase
+	{
+
+		static object locker = new object ();
+		SQLiteConnection database;
+
+		public ItemRefeicaoDataBase ()
+		{
+			database = DependencyService.Get<ISQLite> ().GetConnection ();				
+			database.CreateTable<ItemRefeicao> ();
+		}
+
+		public IEnumerable<ItemRefeicao> GetItems ()
+		{
+			lock (locker) {
+				return (from i in database.Table<ItemRefeicao> ()
+				        select i).ToList ();
+			}
+		}			
+
+		public ItemRefeicao GetItem (int idItemRefeicao)
+		{
+			lock (locker) {
+				return database.Table<ItemRefeicao> (). FirstOrDefault (x => x.id == idItemRefeicao);
+			}
+		}
+
+		public int SaveItem (ItemRefeicao item)
+		{
+			lock (locker) {
+				if (item.id != 0) {
+					database.Update (item);
+					return item.id;
+				} else {
+					return database.Insert (item);
+				}
+			}
+		}
+
+		public int DeleteItem (int id)
+		{
+			lock (locker) {
+				return database.Delete<ItemRefeicao> (id);
+			}
+		}
+	}
+}
---
+++ Dietando/Dietando/Data/UsuarioDataBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using SQLite.Net;
+using Xamarin.Forms;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dietando
+{
+	public class UsuarioDataBase
+	{
+		SQLiteConnection database;
+		static object locker = new object ();
+
+		public UsuarioDataBase ()
+		{
+			database = DependencyService.Get<ISQLite> ().GetConnection ();
+			database.CreateTable<Usuario> ();
+		}
+
+		public IEnumerable<Usuario> GetItems ()
+		{
+			lock (locker) {
+				return (from i in database.Table<Usuario> ()
+				        select i).ToList ();
+			}
+		}
+
+		public Usuario GetItem (int id)
+		{
+			lock (locker) {
+				return database.Table<Usuario> ().FirstOrDefault (x => x.Id == id);
+			}
+		}
+
+		public int DeleteItem (int id)
+		{
+			return database.Delete<Usuario> (id);
+		}
+
+		public int InsertUsuario (Usuario newUsuario)
+		{
+			lock (locker) {
+				return database.Insert (newUsuario);
+			}
+		}
+	}
+}
+
